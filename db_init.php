840b85d2b60fd377c339befba06a7e2806d3200b
Sidney Rutledge <rutledge.sidney@gmail.com>
2023-09-29 15:53:37 -0500

    Add Bootstrap to db_init.php

--- db_init.php
+++ db_init.php
@@ -1,42 +1,63 @@
-<?php
-try {
-    require 'config/db_cfg.php';
-}
-catch(Error $e) {
-    echo "Database configuration file cannot be loaded: " . $e->getMessage();
-    die();
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Database Initialization - Hobby Harmony</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+    <?php
+    try {
+        require 'config/db_cfg.php';
+    }
+    catch(Error $e) {
+        echo
+        "<div class='alert alert-danger'>
+        <strong>Error:</strong> Database configuration file cannot be loaded: " . $e->getMessage() .
+        "</div>";
+        die();
+    }
 
-try {
-    // Create database
-    $conn = new PDO("mysql:host=$dbhost", $dbuser, $dbpass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $sql = "CREATE DATABASE IF NOT EXISTS `$dbname`";
-    $conn->exec($sql);
-    echo "Database &quot;" . $dbname . "&quot; created successfully.<br>";
+    try {
+        // Create database
+        $conn = new PDO("mysql:host=$dbhost", $dbuser, $dbpass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $sql = "CREATE DATABASE IF NOT EXISTS `$dbname`";
+        $conn->exec($sql);
+        echo
+        "<div class='alert alert-success'>
+        Database &quot;" . $dbname . "&quot; created successfully.
+        </div>";
 
-    // Create tables
-    $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Create tables
+        $sql = "USE `$dbname`;
+        CREATE TABLE IF NOT EXISTS `assets` (
+        `ID` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `FriendlyName` varchar(255) NOT NULL,
+        `Manufacturer` varchar(255),
+        `Model` varchar(255),
+        `AssetType` varchar(255),
+        `Location` varchar(255)
+        )";
+        $conn->exec($sql);
+        echo
+        "<div class='alert alert-success'>
+        Table &quot;assets&quot; created successfully.
+        </div>";
+    }
+    catch(PDOException $e) {
+        echo
+        "<div class='alert alert-danger'>
+        <strong>Error:</strong> " . $sql . "<br>" . $e->getMessage() .
+        "</div>";
+    }
 
-    $sql = "USE `$dbname`;
-    CREATE TABLE IF NOT EXISTS `assets` (
-    `ID` int(11) NOT NULL AUTO_INCREMENT,
-    `FriendlyName` varchar(255) NOT NULL,
-    `Manufacturer` varchar(255) DEFAULT NULL,
-    `Model` varchar(255) DEFAULT NULL,
-    `AssetType` varchar(255) DEFAULT NULL,
-    `Location` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`ID`)
-    )";
-    $conn->exec($sql);
-    echo "Table &quot;assets&quot; created successfully.<br>";
-
-}
-catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
-}
-
-$conn = null;
-?>
+    $conn = null;
+    ?>
+    </div>
+</body>
+</html>
--- db_insert_test.php
+++ db_insert_test.php
@@ -1,27 +1,33 @@
+<!DOCTYPE html>
 <?php
-require 'config/db_cfg.php';
+try {
+    require 'config/db_cfg.php';
+}
+catch(Error $e) {
+    echo "Database configuration file cannot be loaded: " . $e->getMessage();
+    die();
+}
 
 // This somehow lets us render a table, but I don't really understand it
 class TableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-      parent::__construct($it, self::LEAVES_ONLY);
-    }
-  
-    function current() {
-      return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-    }
-  
-    function beginChildren() {
-      echo "<tr>";
-    }
-  
-    function endChildren() {
-      echo "</tr>" . "\n";
-    }
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
   }
+  
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+  
+  function beginChildren() {
+    echo "<tr>";
+  }
+  
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
 
 // We're gonna add a MacBook, then try to pull the list
-
 try {
     $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
