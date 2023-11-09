5577e646fd8c88beb0dbda9e379639bc0f71eaf3
Sidney Rutledge <rutledge.sidney@gmail.com>
2023-11-08 22:44:47 -0600

    Remove data-detail-view

--- show_categories.php
+++ show_categories.php
@@ -21,7 +21,7 @@
       <table id="table" class="table table-striped table-responsive-md" 
       data-toggle="table" data-url="retr_categories.php" data-pagination="true" data-search="true"
       data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-show-columns-toggle-all="true"
-      data-detail-view="true" data-show-export="true" data-id-field="ID" data-page-list="[10, 25, 50, 100, all]">
+      data-show-export="true" data-id-field="ID" data-page-list="[10, 25, 50, 100, all]">
         <thead>
           <tr>
             <th data-field="ID" data-sortable="true">ID</th>
